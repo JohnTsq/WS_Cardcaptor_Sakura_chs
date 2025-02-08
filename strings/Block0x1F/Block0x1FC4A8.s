9f919882cb670c9d9ce066a8c7be68fc4efa31f5
JohnTsq <msn451518580@hotmail.com>
2025-02-08 23:42:13 +0800

    新增导出文本

---
+++ strings/Block0x1F/Block0x1FC4A8.s
@@ -0,0 +1,261 @@
+;Block0x1FC4A8
+;子文件文本，共26条子文本
+.org 0x1FC3A0 :: .area 0x1FC4A8-.,0xFF  ;原位覆写须限定area
+
+
+.align 2,0xFF :: Block0x1FC4A8_000:
+;－－－－－－－－－－－－
+;.strn "風よ{结束}"
+;－－－－－－－－－－－－
+ .strn "風よ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_001:
+;－－－－－－－－－－－－
+;.strn "かけﾞよ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "かけﾞよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_002:
+;－－－－－－－－－－－－
+;.strn "水よ{结束}"
+;－－－－－－－－－－－－
+ .strn "水よ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_003:
+;－－－－－－－－－－－－
+;.strn "雷よ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "雷よ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_004:
+;－－－－－－－－－－－－
+;.strn "剣よ{结束}"
+;－－－－－－－－－－－－
+ .strn "剣よ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_005:
+;－－－－－－－－－－－－
+;.strn "あらしよ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "あらしよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_006:
+;－－－－－－－－－－－－
+;.strn "魔力をひめし一撃よ{结束}"
+;－－－－－－－－－－－－
+ .strn "魔力をひめし一撃よ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_007:
+;－－－－－－－－－－－－
+;.strn "こおりよ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "こおりよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_008:
+;－－－－－－－－－－－－
+;.strn "ほのおよ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ほのおよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_009:
+;－－－－－－－－－－－－
+;.strn "ゆきよ{结束}"
+;－－－－－－－－－－－－
+ .strn "ゆきよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_010:
+;－－－－－－－－－－－－
+;.strn "すなよ{结束}"
+;－－－－－－－－－－－－
+ .strn "すなよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_011:
+;－－－－－－－－－－－－
+;.strn "つちよ{结束}"
+;－－－－－－－－－－－－
+ .strn "つちよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_012:
+;－－－－－－－－－－－－
+;.strn "やよ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "やよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_013:
+;－－－－－－－－－－－－
+;.strn "いましめのくさりとなれ！{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "いましめのくさりとなれ！{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_014:
+;－－－－－－－－－－－－
+;.strn "かのものをつつみこめ！{结束}"
+;－－－－－－－－－－－－
+ .strn "かのものをつつみこめ！{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_015:
+;－－－－－－－－－－－－
+;.strn "全てのけかﾞれをおしなかﾞせ！{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "全てのけかﾞれをおしなかﾞせ！{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_016:
+;－－－－－－－－－－－－
+;.strn "さはﾞきのいかすﾞちとなれ！{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "さはﾞきのいかすﾞちとなれ！{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_017:
+;－－－－－－－－－－－－
+;.strn "かのものをうちくたﾞけ！{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "かのものをうちくたﾞけ！{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_018:
+;－－－－－－－－－－－－
+;.strn "すへﾞてのものをふきとはﾞせ！{结束}"
+;－－－－－－－－－－－－
+ .strn "すへﾞてのものをふきとはﾞせ！{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_019:
+;－－－－－－－－－－－－
+;.strn "せいなるくさひﾞとなれ！{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "せいなるくさひﾞとなれ！{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_020:
+;－－－－－－－－－－－－
+;.strn "けかﾞれなきやいはﾞとなれ！{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "けかﾞれなきやいはﾞとなれ！{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_021:
+;－－－－－－－－－－－－
+;.strn "しんはﾞつのつるきﾞとなれ！{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "しんはﾞつのつるきﾞとなれ！{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_022:
+;－－－－－－－－－－－－
+;.strn "れいてつなるあらしとなれ！{结束}"
+;－－－－－－－－－－－－
+ .strn "れいてつなるあらしとなれ！{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_023:
+;－－－－－－－－－－－－
+;.strn "かのものをひきつﾞりこめ！{结束}"
+;－－－－－－－－－－－－
+ .strn "かのものをひきつﾞりこめ！{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_024:
+;－－－－－－－－－－－－
+;.strn "たけりくるうりゅうとなれ！{结束}"
+;－－－－－－－－－－－－
+ .strn "たけりくるうりゅうとなれ！{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC4A8_025:
+;－－－－－－－－－－－－
+;.strn "魔をつらぬくしらはとなれ！{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "魔をつらぬくしらはとなれ！{结束}"
+;－－－－－－－－－－－－
+.endarea
+
+
+;子文件文本指针
+.org 0x1FC4A8   ;暂勿改动指针所处地址
+        .hword (Block0x1FC4A8_000 & 0xF),(((Block0x1FC4A8_000 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC4B0
+        .hword (Block0x1FC4A8_001 & 0xF),(((Block0x1FC4A8_001 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC4A8_002 & 0xF),(((Block0x1FC4A8_002 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC4BC
+        .hword (Block0x1FC4A8_003 & 0xF),(((Block0x1FC4A8_003 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC4A8_004 & 0xF),(((Block0x1FC4A8_004 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC4CC
+        .hword (Block0x1FC4A8_005 & 0xF),(((Block0x1FC4A8_005 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC4DC
+        .hword (Block0x1FC4A8_006 & 0xF),(((Block0x1FC4A8_006 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC4E4
+        .hword (Block0x1FC4A8_007 & 0xF),(((Block0x1FC4A8_007 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC4A8_008 & 0xF),(((Block0x1FC4A8_008 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC4A8_009 & 0xF),(((Block0x1FC4A8_009 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC4F4
+        .hword (Block0x1FC4A8_010 & 0xF),(((Block0x1FC4A8_010 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC4FC
+        .hword (Block0x1FC4A8_011 & 0xF),(((Block0x1FC4A8_011 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC508
+        .hword (Block0x1FC4A8_012 & 0xF),(((Block0x1FC4A8_012 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC4A8_013 & 0xF),(((Block0x1FC4A8_013 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC514
+        .hword (Block0x1FC4A8_014 & 0xF),(((Block0x1FC4A8_014 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC4A8_015 & 0xF),(((Block0x1FC4A8_015 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC520
+        .hword (Block0x1FC4A8_016 & 0xF),(((Block0x1FC4A8_016 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC4A8_017 & 0xF),(((Block0x1FC4A8_017 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC530
+        .hword (Block0x1FC4A8_018 & 0xF),(((Block0x1FC4A8_018 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC540
+        .hword (Block0x1FC4A8_019 & 0xF),(((Block0x1FC4A8_019 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC548
+        .hword (Block0x1FC4A8_020 & 0xF),(((Block0x1FC4A8_020 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC4A8_021 & 0xF),(((Block0x1FC4A8_021 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC4A8_022 & 0xF),(((Block0x1FC4A8_022 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC558
+        .hword (Block0x1FC4A8_023 & 0xF),(((Block0x1FC4A8_023 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC560
+        .hword (Block0x1FC4A8_024 & 0xF),(((Block0x1FC4A8_024 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC56C
+        .hword (Block0x1FC4A8_025 & 0xF),(((Block0x1FC4A8_025 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+
+
+.func Block0x1FC4A8_End :: .endfunc
---
+++ strings/Block0x1F/Block0x1FC58A.s
@@ -0,0 +1,198 @@
+;Block0x1FC58A
+;子文件文本，共35条子文本
+.org 0x1FC570 :: .area 0x1FC58A-.,0xFF
+.align 2,0xFF :: Block0x1FC58A_000:
+;.strn "汝のあるへﾞき姿に戻れ！{结束}"
+ .strn "汝のあるへﾞき姿に戻れ！{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_001:
+;.strn "クロウカートﾞ！{结束}"
+ .strn "クロウカートﾞ！{结束}"
+.endarea
+
+.org 0x1FC592 :: .area 0x1FC59E-.,0xFF
+.align 2,0xFF :: Block0x1FC58A_002:
+;.strn "日っきをつけるんやな{换行}{结束}"
+ .strn "日っきをつけるんやな{换行}{结束}"
+.endarea
+
+.org 0x1FC5A2 :: .area 0x1FC5BC-.,0xFF
+.align 2,0xFF :: Block0x1FC58A_003:
+;.strn "汝のあるへﾞき姿にもとﾞれ{结束}"
+ .strn "汝のあるへﾞき姿にもとﾞれ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_004:
+;.strn "クロウカートﾞ！{结束}"
+ .strn "クロウカートﾞ！{结束}"
+.endarea
+
+.org 0x1FC5C4 :: .area 0x1FC5E8-.,0xFF
+.align 2,0xFF :: Block0x1FC58A_005:
+;.strn "闇の力をひめし鍵よ{结束}"
+ .strn "闇の力をひめし鍵よ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_006:
+;.strn "真の姿を我の前に示せ{结束}"
+ .strn "真の姿を我の前に示せ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_007:
+;.strn "レリースﾞ{结束}"
+ .strn "レリースﾞ{结束}"
+.endarea
+
+.org 0x1FC5F6 :: .area 0x1FC6B2-.,0xFF
+.align 2,0xFF :: Block0x1FC58A_008:
+;.strn "オーフﾟニンクﾞ{结束}"
+ .strn "オーフﾟニンクﾞ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_009:
+;.strn "夢{结束}"
+ .strn "夢{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_010:
+;.strn "育成{结束}"
+ .strn "育成{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_011:
+;.strn "にちしﾞょう{结束}"
+ .strn "にちしﾞょう{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_012:
+;.strn "ほのほﾞの{结束}"
+ .strn "ほのほﾞの{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_013:
+;.strn "きょうふ{结束}"
+ .strn "きょうふ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_014:
+;.strn "おまぬけ{结束}"
+ .strn "おまぬけ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_015:
+;.strn "あこかﾞれ{结束}"
+ .strn "あこかﾞれ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_016:
+;.strn "ハﾟニック{结束}"
+ .strn "ハﾟニック{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_017:
+;.strn "きんはﾟく{结束}"
+ .strn "きんはﾟく{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_018:
+;.strn "ひあい{结束}"
+ .strn "ひあい{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_019:
+;.strn "ヒﾟンチ{结束}"
+ .strn "ヒﾟンチ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_020:
+;.strn "お出かけ{结束}"
+ .strn "お出かけ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_021:
+;.strn "夜まつり{结束}"
+ .strn "夜まつり{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_022:
+;.strn "雪兔とテﾞート{结束}"
+ .strn "雪兔とテﾞート{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_023:
+;.strn "クロウカートﾞ{结束}"
+ .strn "クロウカートﾞ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_024:
+;.strn "封印解除{结束}"
+ .strn "封印解除{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_025:
+;.strn "戦闘{结束}"
+ .strn "戦闘{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_026:
+;.strn "カートﾞ封印{结束}"
+ .strn "カートﾞ封印{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_027:
+;.strn "最後の審判{结束}"
+ .strn "最後の審判{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_028:
+;.strn "ラスホﾞス{结束}"
+ .strn "ラスホﾞス{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_029:
+;.strn "勝利{结束}"
+ .strn "勝利{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_030:
+;.strn "ショッフﾟ{结束}"
+ .strn "ショッフﾟ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_031:
+;.strn "知世の歌{结束}"
+ .strn "知世の歌{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_032:
+;.strn "ふえ{结束}"
+ .strn "ふえ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_033:
+;.strn "ハﾞットﾞエントﾞ{结束}"
+ .strn "ハﾞットﾞエントﾞ{结束}"
+
+.align 2,0xFF :: Block0x1FC58A_034:
+;.strn "エンテﾞィンクﾞ{结束}"
+ .strn "エンテﾞィンクﾞ{结束}"
+.endarea
+
+;子文件文本指针
+.org 0x1FC58A   ;暂勿改动指针所处地址
+        .hword (Block0x1FC58A_000 & 0xF),(((Block0x1FC58A_000 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_001 & 0xF),(((Block0x1FC58A_001 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC59E
+        .hword (Block0x1FC58A_002 & 0xF),(((Block0x1FC58A_002 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC5BC
+        .hword (Block0x1FC58A_003 & 0xF),(((Block0x1FC58A_003 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_004 & 0xF),(((Block0x1FC58A_004 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.org 0x1FC5E8
+        .hword (Block0x1FC58A_005 & 0xF),(((Block0x1FC58A_005 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_006 & 0xF),(((Block0x1FC58A_006 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_007 & 0xF),(((Block0x1FC58A_007 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword 0xFFFF
+.org 0x1FC6B2
+        .hword (Block0x1FC58A_008 & 0xF),(((Block0x1FC58A_008 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_009 & 0xF),(((Block0x1FC58A_009 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_010 & 0xF),(((Block0x1FC58A_010 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_011 & 0xF),(((Block0x1FC58A_011 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_012 & 0xF),(((Block0x1FC58A_012 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_013 & 0xF),(((Block0x1FC58A_013 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_014 & 0xF),(((Block0x1FC58A_014 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_015 & 0xF),(((Block0x1FC58A_015 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_016 & 0xF),(((Block0x1FC58A_016 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_017 & 0xF),(((Block0x1FC58A_017 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_018 & 0xF),(((Block0x1FC58A_018 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_019 & 0xF),(((Block0x1FC58A_019 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_020 & 0xF),(((Block0x1FC58A_020 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_021 & 0xF),(((Block0x1FC58A_021 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_022 & 0xF),(((Block0x1FC58A_022 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_023 & 0xF),(((Block0x1FC58A_023 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_024 & 0xF),(((Block0x1FC58A_024 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_025 & 0xF),(((Block0x1FC58A_025 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_026 & 0xF),(((Block0x1FC58A_026 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_027 & 0xF),(((Block0x1FC58A_027 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_028 & 0xF),(((Block0x1FC58A_028 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_029 & 0xF),(((Block0x1FC58A_029 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_030 & 0xF),(((Block0x1FC58A_030 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_031 & 0xF),(((Block0x1FC58A_031 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_032 & 0xF),(((Block0x1FC58A_032 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_033 & 0xF),(((Block0x1FC58A_033 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC58A_034 & 0xF),(((Block0x1FC58A_034 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.endarea
+
+
+.func Block0x1FC58A_End :: .endfunc
