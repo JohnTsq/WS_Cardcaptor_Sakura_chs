2fa9d26627d9d80134e79684cecfd014c487b35e
JohnTsq <msn451518580@hotmail.com>
2025-02-09 17:48:52 +0800

    新增导出文本

---
+++ strings/Block0x1F/Block0x1FD144.s
@@ -0,0 +1,476 @@
+;Block0x1FD144
+;子文件文本，共41条子文本
+.org 0x1FCD6C :: .area 0x1FD144-.,0xFF  ;原位覆写须限定area
+
+
+.align 2,0xFF :: Block0x1FD144_000:
+;－－－－－－－－－－－－
+;.strn "とﾞれを使うの？{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "とﾞれを使うの？{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_001:
+;－－－－－－－－－－－－
+;.strn "左右てﾞ選んてﾞⒶホﾞタンてﾞ決定{换行}"
+;.strn "Ⓑホﾞタンてﾞキャンセルたﾞよ｡{结束}"
+;－－－－－－－－－－－－
+ .strn "左右てﾞ選んてﾞⒶホﾞタンてﾞ決定{换行}"
+ .strn "Ⓑホﾞタンてﾞキャンセルたﾞよ｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_002:
+;－－－－－－－－－－－－
+;.strn "これを使うの？{结束}"
+;－－－－－－－－－－－－
+ .strn "これを使うの？{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_003:
+;－－－－－－－－－－－－
+;.strn "これは､今は大切に取って{换行}"
+;.strn "おこうっと{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "これは､今は大切に取って{换行}"
+ .strn "おこうっと{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_004:
+;－－－－－－－－－－－－
+;.strn "　使う{换行}"
+;.strn "　使わない{结束}"
+;－－－－－－－－－－－－
+ .strn "　使う{换行}"
+ .strn "　使わない{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_005:
+;－－－－－－－－－－－－
+;.strn "……………………｡{结束}"
+;－－－－－－－－－－－－
+ .strn "……………………｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_006:
+;－－－－－－－－－－－－
+;.strn "『ちしき』かﾞ上かﾞったよ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "『ちしき』かﾞ上かﾞったよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_007:
+;－－－－－－－－－－－－
+;.strn "『かんせい』かﾞ上かﾞったよ{结束}"
+;－－－－－－－－－－－－
+ .strn "『かんせい』かﾞ上かﾞったよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_008:
+;－－－－－－－－－－－－
+;.strn "『たいりょく』かﾞ上かﾞったよ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "『たいりょく』かﾞ上かﾞったよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_009:
+;－－－－－－－－－－－－
+;.strn "『はやさ』かﾞ上かﾞったよ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "『はやさ』かﾞ上かﾞったよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_010:
+;－－－－－－－－－－－－
+;.strn "『つかれ』かﾞさかﾞったよ{结束}"
+;－－－－－－－－－－－－
+ .strn "『つかれ』かﾞさかﾞったよ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_011:
+;－－－－－－－－－－－－
+;.strn "チョコの材料{结束}"
+;－－－－－－－－－－－－
+ .strn "チョコの材料{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_012:
+;－－－－－－－－－－－－
+;.strn "手作りチョコ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "手作りチョコ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_013:
+;－－－－－－－－－－－－
+;.strn "チョコ{结束}"
+;－－－－－－－－－－－－
+ .strn "チョコ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_014:
+;－－－－－－－－－－－－
+;.strn "思いいし{结束}"
+;－－－－－－－－－－－－
+ .strn "思いいし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_015:
+;－－－－－－－－－－－－
+;.strn "手鏡とフﾞラシ{结束}"
+;－－－－－－－－－－－－
+ .strn "手鏡とフﾞラシ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_016:
+;－－－－－－－－－－－－
+;.strn "リホﾞン{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "リホﾞン{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_017:
+;－－－－－－－－－－－－
+;.strn "しおりとカハﾞー{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "しおりとカハﾞー{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_018:
+;－－－－－－－－－－－－
+;.strn "ハンカチ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ハンカチ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_019:
+;－－－－－－－－－－－－
+;.strn "カーネイション{结束}"
+;－－－－－－－－－－－－
+ .strn "カーネイション{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_020:
+;－－－－－－－－－－－－
+;.strn "きしﾞ(生地){结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "きしﾞ(生地){结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_021:
+;－－－－－－－－－－－－
+;.strn "なへﾞつかみ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "なへﾞつかみ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_022:
+;－－－－－－－－－－－－
+;.strn "さんこう書{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "さんこう書{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_023:
+;－－－－－－－－－－－－
+;.strn "音楽ＣＤ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "音楽ＣＤ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_024:
+;－－－－－－－－－－－－
+;.strn "なわとひﾞ{结束}"
+;－－－－－－－－－－－－
+ .strn "なわとひﾞ{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_025:
+;－－－－－－－－－－－－
+;.strn "ともたﾞち{结束}"
+;－－－－－－－－－－－－
+ .strn "ともたﾞち{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_026:
+;－－－－－－－－－－－－
+;.strn "チョコの材料{换行}"
+;.strn "手作りチョコを作るため{换行}"
+;.strn "の材料｡あとは､こころを{换行}"
+;.strn "込めて作るたﾞけ｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "チョコの材料{换行}"
+ .strn "手作りチョコを作るため{换行}"
+ .strn "の材料｡あとは､こころを{换行}"
+ .strn "込めて作るたﾞけ｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_027:
+;－－－－－－－－－－－－
+;.strn "チョコ{换行}"
+;.strn "手作りチョコ｡こころを{换行}"
+;.strn "込めて作ってあるから､{换行}"
+;.strn "もちろんおいしい？{结束}"
+;－－－－－－－－－－－－
+ .strn "チョコ{换行}"
+ .strn "手作りチョコ｡こころを{换行}"
+ .strn "込めて作ってあるから､{换行}"
+ .strn "もちろんおいしい？{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_028:
+;－－－－－－－－－－－－
+;.strn "チョコ{换行}"
+;.strn "お店てﾞ売っているチョコ｡{换行}"
+;.strn "手かﾞるに想いを伝えら{换行}"
+;.strn "れる｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "チョコ{换行}"
+ .strn "お店てﾞ売っているチョコ｡{换行}"
+ .strn "手かﾞるに想いを伝えら{换行}"
+ .strn "れる｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_029:
+;－－－－－－－－－－－－
+;.strn "思いいし{换行}"
+;.strn "ふたつのいし｡ひとつすﾞつ{换行}"
+;.strn "男女かﾞ持つと仲かﾞ良くな{换行}"
+;.strn "るふしきﾞないし｡{结束}"
+;－－－－－－－－－－－－
+ .strn "思いいし{换行}"
+ .strn "ふたつのいし｡ひとつすﾞつ{换行}"
+ .strn "男女かﾞ持つと仲かﾞ良くな{换行}"
+ .strn "るふしきﾞないし｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_030:
+;－－－－－－－－－－－－
+;.strn "手鏡とフﾞラシ{换行}"
+;.strn "命の次に大事なかみの{换行}"
+;.strn "けを美しくととのえる｡{换行}"
+;.strn "かみの長い女の子に｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "手鏡とフﾞラシ{换行}"
+ .strn "命の次に大事なかみの{换行}"
+ .strn "けを美しくととのえる｡{换行}"
+ .strn "かみの長い女の子に｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_031:
+;－－－－－－－－－－－－
+;.strn "リホﾞン{换行}"
+;.strn "かみのけを可愛くいろとﾞ{换行}"
+;.strn "るリホﾞン｡さひﾞしいかみ{换行}"
+;.strn "のけのワンホﾟイントに｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "リホﾞン{换行}"
+ .strn "かみのけを可愛くいろとﾞ{换行}"
+ .strn "るリホﾞン｡さひﾞしいかみ{换行}"
+ .strn "のけのワンホﾟイントに｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_032:
+;－－－－－－－－－－－－
+;.strn "しおりとカハﾞー{换行}"
+;.strn "大切な本を守るカハﾞー{换行}"
+;.strn "と､とﾞこまてﾞ読んたﾞか分{换行}"
+;.strn "かるしおりのセット｡{结束}"
+;－－－－－－－－－－－－
+ .strn "しおりとカハﾞー{换行}"
+ .strn "大切な本を守るカハﾞー{换行}"
+ .strn "と､とﾞこまてﾞ読んたﾞか分{换行}"
+ .strn "かるしおりのセット｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_033:
+;－－－－－－－－－－－－
+;.strn "ハンカチ{换行}"
+;.strn "可愛らしいもようのハン{换行}"
+;.strn "カチ｡ちょっと大人っほﾟい{换行}"
+;.strn "女の子に｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ハンカチ{换行}"
+ .strn "可愛らしいもようのハン{换行}"
+ .strn "カチ｡ちょっと大人っほﾟい{换行}"
+ .strn "女の子に｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_034:
+;－－－－－－－－－－－－
+;.strn "カーネイション{换行}"
+;.strn "母の日といえはﾞこのお花｡{换行}"
+;.strn "かんしゃの気持ちと一緒に{换行}"
+;.strn "おくりましょう｡{结束}"
+;－－－－－－－－－－－－
+ .strn "カーネイション{换行}"
+ .strn "母の日といえはﾞこのお花｡{换行}"
+ .strn "かんしゃの気持ちと一緒に{换行}"
+ .strn "おくりましょう｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_035:
+;－－－－－－－－－－－－
+;.strn "きしﾞ(生地){换行}"
+;.strn "いろいろな物かﾞ作れる{换行}"
+;.strn "きしﾞ｡{结束}"
+;－－－－－－－－－－－－
+ .strn "きしﾞ(生地){换行}"
+ .strn "いろいろな物かﾞ作れる{换行}"
+ .strn "きしﾞ｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_036:
+;－－－－－－－－－－－－
+;.strn "なへﾞつかみ{换行}"
+;.strn "きしﾞから作られたなへﾞ{换行}"
+;.strn "つかみ｡お料理かﾞ得意{换行}"
+;.strn "な人へフﾟレセﾞント｡{结束}"
+;－－－－－－－－－－－－
+ .strn "なへﾞつかみ{换行}"
+ .strn "きしﾞから作られたなへﾞ{换行}"
+ .strn "つかみ｡お料理かﾞ得意{换行}"
+ .strn "な人へフﾟレセﾞント｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_037:
+;－－－－－－－－－－－－
+;.strn "さんこう書{换行}"
+;.strn "学校の予習､ふく習をする{换行}"
+;.strn "ためのテキスト｡{换行}"
+;.strn "ちしきかﾞ少し上かﾞる｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "さんこう書{换行}"
+ .strn "学校の予習､ふく習をする{换行}"
+ .strn "ためのテキスト｡{换行}"
+ .strn "ちしきかﾞ少し上かﾞる｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_038:
+;－－－－－－－－－－－－
+;.strn "音楽ＣＤ{换行}"
+;.strn "ヒットきょくを集めたお{换行}"
+;.strn "得なＣＤ｡{换行}"
+;.strn "感性かﾞ少し上かﾞる｡{结束}"
+;－－－－－－－－－－－－
+ .strn "音楽ＣＤ{换行}"
+ .strn "ヒットきょくを集めたお{换行}"
+ .strn "得なＣＤ｡{换行}"
+ .strn "感性かﾞ少し上かﾞる｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_039:
+;－－－－－－－－－－－－
+;.strn "なわとひﾞ{换行}"
+;.strn "手かﾞるな運動かﾞてﾞきる｡{换行}"
+;.strn "体力かはやさかﾞ少し上かﾞ{换行}"
+;.strn "る｡{结束}"
+;－－－－－－－－－－－－
+ .strn "なわとひﾞ{换行}"
+ .strn "手かﾞるな運動かﾞてﾞきる｡{换行}"
+ .strn "体力かはやさかﾞ少し上かﾞ{换行}"
+ .strn "る｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FD144_040:
+;－－－－－－－－－－－－
+;.strn "ともたﾞち{换行}"
+;.strn "人気まんかﾞかﾞ連さいさ{换行}"
+;.strn "れているコミックさﾞっし｡{换行}"
+;.strn "つかれかﾞ少し下かﾞる｡{结束}"
+;－－－－－－－－－－－－
+ .strn "ともたﾞち{换行}"
+ .strn "人気まんかﾞかﾞ連さいさ{换行}"
+ .strn "れているコミックさﾞっし｡{换行}"
+ .strn "つかれかﾞ少し下かﾞる｡{结束}"
+;－－－－－－－－－－－－
+.endarea
+
+;子文件文本指针
+.org 0x1FD144   ;暂勿改动指针所处地址
+;.align 2,0xFF :: Block0x1FD144_Ptr:
+.area 0x1FD1E8-.        ;原位覆写须限定area
+        .hword (Block0x1FD144_000 & 0xF),(((Block0x1FD144_000 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_001 & 0xF),(((Block0x1FD144_001 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_002 & 0xF),(((Block0x1FD144_002 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_003 & 0xF),(((Block0x1FD144_003 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_004 & 0xF),(((Block0x1FD144_004 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_005 & 0xF),(((Block0x1FD144_005 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_006 & 0xF),(((Block0x1FD144_006 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_007 & 0xF),(((Block0x1FD144_007 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_008 & 0xF),(((Block0x1FD144_008 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_009 & 0xF),(((Block0x1FD144_009 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_010 & 0xF),(((Block0x1FD144_010 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_011 & 0xF),(((Block0x1FD144_011 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_012 & 0xF),(((Block0x1FD144_012 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_013 & 0xF),(((Block0x1FD144_013 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_014 & 0xF),(((Block0x1FD144_014 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_015 & 0xF),(((Block0x1FD144_015 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_016 & 0xF),(((Block0x1FD144_016 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_017 & 0xF),(((Block0x1FD144_017 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_018 & 0xF),(((Block0x1FD144_018 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_019 & 0xF),(((Block0x1FD144_019 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_020 & 0xF),(((Block0x1FD144_020 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_021 & 0xF),(((Block0x1FD144_021 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_022 & 0xF),(((Block0x1FD144_022 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_023 & 0xF),(((Block0x1FD144_023 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_024 & 0xF),(((Block0x1FD144_024 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_025 & 0xF),(((Block0x1FD144_025 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_026 & 0xF),(((Block0x1FD144_026 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_027 & 0xF),(((Block0x1FD144_027 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_028 & 0xF),(((Block0x1FD144_028 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_029 & 0xF),(((Block0x1FD144_029 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_030 & 0xF),(((Block0x1FD144_030 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_031 & 0xF),(((Block0x1FD144_031 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_032 & 0xF),(((Block0x1FD144_032 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_033 & 0xF),(((Block0x1FD144_033 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_034 & 0xF),(((Block0x1FD144_034 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_035 & 0xF),(((Block0x1FD144_035 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_036 & 0xF),(((Block0x1FD144_036 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_037 & 0xF),(((Block0x1FD144_037 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_038 & 0xF),(((Block0x1FD144_038 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_039 & 0xF),(((Block0x1FD144_039 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FD144_040 & 0xF),(((Block0x1FD144_040 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.endarea
+
+
+.func Block0x1FD144_End :: .endfunc
