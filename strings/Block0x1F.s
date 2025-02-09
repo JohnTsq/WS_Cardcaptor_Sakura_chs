c1b166aa46bc520f031ce81253ad65f870bbf3a9
JohnTsq <msn451518580@hotmail.com>
2025-02-09 22:22:12 +0800

    更新Block0x1F内的.include

--- strings/Block0x1F.s
+++ strings/Block0x1F.s
@@ -3,7 +3,6 @@ OffsetOfBlock0x1FInRamSegment   equ 0xF000
 
 ;子文件
 .include ".\strings\Block0x1F\Block0x1F6670.s"
-;.include ".\strings\Block0x1F\Block0x1F678A.s"
 .include ".\strings\Block0x1F\Block0x1F69E2.s"
 .include ".\strings\Block0x1F\Block0x1F7762.s"
 .include ".\strings\Block0x1F\Block0x1F7D84.s"
@@ -13,15 +12,15 @@ OffsetOfBlock0x1FInRamSegment   equ 0xF000
 .include ".\strings\Block0x1F\Block0x1F91E2.s"
 .include ".\strings\Block0x1F\Block0x1FA13A.s"
 .include ".\strings\Block0x1F\Block0x1FA1BC.s"
-;.include ".\strings\Block0x1F\Block0x1FADBC.s"
-;.include ".\strings\Block0x1F\Block0x1FB1B2.s"
-;.include ".\strings\Block0x1F\Block0x1FBA42.s"
-;.include ".\strings\Block0x1F\Block0x1FC210.s"
-;.include ".\strings\Block0x1F\Block0x1FC314.s"
-;.include ".\strings\Block0x1F\Block0x1FC396.s"
-;.include ".\strings\Block0x1F\Block0x1FC4A8.s"
+.include ".\strings\Block0x1F\Block0x1FADBC.s"
+.include ".\strings\Block0x1F\Block0x1FB1B2.s"
+.include ".\strings\Block0x1F\Block0x1FBA46.s"
+.include ".\strings\Block0x1F\Block0x1FC210.s"
+.include ".\strings\Block0x1F\Block0x1FC314.s"
+.include ".\strings\Block0x1F\Block0x1FC4A8.s"
 .include ".\strings\Block0x1F\Block0x1FC58A.s"
+;staff list
 ;.include ".\strings\Block0x1F\Block0x1FCB58.s"
-;.include ".\strings\Block0x1F\Block0x1FD144.s"
+.include ".\strings\Block0x1F\Block0x1FD144.s"
 
 .func Block0x1F_End :: .endfunc
