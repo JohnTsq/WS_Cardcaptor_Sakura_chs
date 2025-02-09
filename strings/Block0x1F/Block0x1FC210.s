1a6e101420a4fb5aead31d84b8683032b39f7a9e
JohnTsq <msn451518580@hotmail.com>
2025-02-09 17:18:31 +0800

    新增导出文本

---
+++ strings/Block0x1F/Block0x1FC210.s
@@ -0,0 +1,749 @@
+;Block0x1FC210
+;子文件文本，共52条子文本
+.org 0x1FBBBA :: .area 0x1FC210-.,0xFF  ;原位覆写须限定area
+
+
+.align 2,0xFF :: Block0x1FC210_000:
+;－－－－－－－－－－－－
+;.strn "ウィンテﾞィ{换行}"
+;.strn "　いこﾞ､風クﾞルーフﾟ{换行}"
+;.strn "のカートﾞのＥに１５{换行}"
+;.strn "をたす{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ウィンテﾞィ{换行}"
+ .strn "　いこﾞ､風クﾞルーフﾟ{换行}"
+ .strn "のカートﾞのＥに１５{换行}"
+ .strn "をたす{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_001:
+;－－－－－－－－－－－－
+;.strn "フライ{换行}"
+;.strn "　いこﾞ､３ターン{换行}"
+;.strn "地クﾞルーフﾟのカー{换行}"
+;.strn "トﾞのタﾞメーシﾞから{换行}"
+;.strn "３０かﾞひかれる{结束}"
+;－－－－－－－－－－－－
+ .strn "フライ{换行}"
+ .strn "　いこﾞ､３ターン{换行}"
+ .strn "地クﾞルーフﾟのカー{换行}"
+ .strn "トﾞのタﾞメーシﾞから{换行}"
+ .strn "３０かﾞひかれる{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_002:
+;－－－－－－－－－－－－
+;.strn "シャトﾞウ{换行}"
+;.strn "　特になし{结束}"
+;－－－－－－－－－－－－
+ .strn "シャトﾞウ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_003:
+;－－－－－－－－－－－－
+;.strn "ウォーティ{换行}"
+;.strn "　いこﾞ､水クﾞルーフﾟ{换行}"
+;.strn "のカートﾞのＥに１５{换行}"
+;.strn "をたす{结束}"
+;－－－－－－－－－－－－
+ .strn "ウォーティ{换行}"
+ .strn "　いこﾞ､水クﾞルーフﾟ{换行}"
+ .strn "のカートﾞのＥに１５{换行}"
+ .strn "をたす{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_004:
+;－－－－－－－－－－－－
+;.strn "レイン{换行}"
+;.strn "　しﾞせﾞんに､クラウ{换行}"
+;.strn "トﾞを使っておくと､{换行}"
+;.strn "Ｅに５０かﾞたされる{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "レイン{换行}"
+ .strn "　しﾞせﾞんに､クラウ{换行}"
+ .strn "トﾞを使っておくと､{换行}"
+ .strn "Ｅに５０かﾞたされる{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_005:
+;－－－－－－－－－－－－
+;.strn "ウットﾞ{换行}"
+;.strn "　しﾞせﾞんに､レイン{换行}"
+;.strn "を使っておくと､Ｅに{换行}"
+;.strn "５０かﾞたされ{换行}"
+;.strn "る{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ウットﾞ{换行}"
+ .strn "　しﾞせﾞんに､レイン{换行}"
+ .strn "を使っておくと､Ｅに{换行}"
+ .strn "５０かﾞたされ{换行}"
+ .strn "る{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_006:
+;－－－－－－－－－－－－
+;.strn "シﾞャンフﾟ{换行}"
+;.strn "　特になし{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "シﾞャンフﾟ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_007:
+;－－－－－－－－－－－－
+;.strn "イリューシﾞョン{换行}"
+;.strn "　いこﾞ､水クﾞルーフﾟ{换行}"
+;.strn "のほﾞうきﾞょカートﾞの{换行}"
+;.strn "Ｅに３０をたす{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "イリューシﾞョン{换行}"
+ .strn "　いこﾞ､水クﾞルーフﾟ{换行}"
+ .strn "のほﾞうきﾞょカートﾞの{换行}"
+ .strn "Ｅに３０をたす{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_008:
+;－－－－－－－－－－－－
+;.strn "サイレント{换行}"
+;.strn "　いこﾞ､３ターン相{换行}"
+;.strn "手火クﾞルーフﾟの{换行}"
+;.strn "カートﾞのＥから２５{换行}"
+;.strn "かﾞひかれる{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "サイレント{换行}"
+ .strn "　いこﾞ､３ターン相{换行}"
+ .strn "手火クﾞルーフﾟの{换行}"
+ .strn "カートﾞのＥから２５{换行}"
+ .strn "かﾞひかれる{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_009:
+;－－－－－－－－－－－－
+;.strn "サンタﾞー{换行}"
+;.strn "　同しﾞターン､相{换行}"
+;.strn "手かﾞシャトﾞウを使{换行}"
+;.strn "うとタﾞメーシﾞかﾞ２{换行}"
+;.strn "分の１になってしま{换行}"
+;.strn "う{结束}"
+;－－－－－－－－－－－－
+ .strn "サンタﾞー{换行}"
+ .strn "　同しﾞターン､相{换行}"
+ .strn "手かﾞシャトﾞウを使{换行}"
+ .strn "うとタﾞメーシﾞかﾞ２{换行}"
+ .strn "分の１になってしま{换行}"
+ .strn "う{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_010:
+;－－－－－－－－－－－－
+;.strn "ソートﾞ{换行}"
+;.strn "　特になし{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ソートﾞ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_011:
+;－－－－－－－－－－－－
+;.strn "フラワー{换行}"
+;.strn "　特になし{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "フラワー{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_012:
+;－－－－－－－－－－－－
+;.strn "シールトﾞ{换行}"
+;.strn "　特になし{结束}"
+;－－－－－－－－－－－－
+ .strn "シールトﾞ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_013:
+;－－－－－－－－－－－－
+;.strn "タイム{换行}"
+;.strn "　いこﾞ､風クﾞルーフﾟ{换行}"
+;.strn "のカートﾞのＥに２５{换行}"
+;.strn "をたす{结束}"
+;－－－－－－－－－－－－
+ .strn "タイム{换行}"
+ .strn "　いこﾞ､風クﾞルーフﾟ{换行}"
+ .strn "のカートﾞのＥに２５{换行}"
+ .strn "をたす{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_014:
+;－－－－－－－－－－－－
+;.strn "ハﾟワー{换行}"
+;.strn "　特になし{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ハﾟワー{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_015:
+;－－－－－－－－－－－－
+;.strn "ミスト{换行}"
+;.strn "　イリューシﾞョンを{换行}"
+;.strn "使っておくと､Ｅに{换行}"
+;.strn "７０たされる{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ミスト{换行}"
+ .strn "　イリューシﾞョンを{换行}"
+ .strn "使っておくと､Ｅに{换行}"
+ .strn "７０たされる{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_016:
+;－－－－－－－－－－－－
+;.strn "フロート{换行}"
+;.strn "　いこﾞ､５ターン相{换行}"
+;.strn "手地クﾞルーフﾟのカ{换行}"
+;.strn "ートﾞのタﾞメーシﾞかﾞ{换行}"
+;.strn "３０ひかれる{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "フロート{换行}"
+ .strn "　いこﾞ､５ターン相{换行}"
+ .strn "手地クﾞルーフﾟのカ{换行}"
+ .strn "ートﾞのタﾞメーシﾞかﾞ{换行}"
+ .strn "３０ひかれる{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_017:
+;－－－－－－－－－－－－
+;.strn "イレイスﾞ{换行}"
+;.strn "　特になし{结束}"
+;－－－－－－－－－－－－
+ .strn "イレイスﾞ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_018:
+;－－－－－－－－－－－－
+;.strn "クﾞロウ{换行}"
+;.strn "　特になし{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "クﾞロウ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_019:
+;－－－－－－－－－－－－
+;.strn "ムーフﾞ{换行}"
+;.strn "　特になし{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ムーフﾞ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_020:
+;－－－－－－－－－－－－
+;.strn "ファイト{换行}"
+;.strn "　いこﾞ､自分の使用{换行}"
+;.strn "する攻撃カートﾞ全て{换行}"
+;.strn "のＥに２０たす{结束}"
+;－－－－－－－－－－－－
+ .strn "ファイト{换行}"
+ .strn "　いこﾞ､自分の使用{换行}"
+ .strn "する攻撃カートﾞ全て{换行}"
+ .strn "のＥに２０たす{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_021:
+;－－－－－－－－－－－－
+;.strn "ルーフﾟ{换行}"
+;.strn "　特になし{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ルーフﾟ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_022:
+;－－－－－－－－－－－－
+;.strn "スリーフﾟ{换行}"
+;.strn "　いこﾞ､３ターン相{换行}"
+;.strn "手の水クﾞルーフﾟの{换行}"
+;.strn "カートﾞのＥから４０{换行}"
+;.strn "をひく{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "スリーフﾟ{换行}"
+ .strn "　いこﾞ､３ターン相{换行}"
+ .strn "手の水クﾞルーフﾟの{换行}"
+ .strn "カートﾞのＥから４０{换行}"
+ .strn "をひく{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_023:
+;－－－－－－－－－－－－
+;.strn "ソンクﾞ{换行}"
+;.strn "　特になし{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ソンクﾞ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_024:
+;－－－－－－－－－－－－
+;.strn "リトル{换行}"
+;.strn "　いこﾞ､３ターンほﾞ{换行}"
+;.strn "うきﾞょしたときの{换行}"
+;.strn "タﾞメーシﾞを２分の１{换行}"
+;.strn "にへらす{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "リトル{换行}"
+ .strn "　いこﾞ､３ターンほﾞ{换行}"
+ .strn "うきﾞょしたときの{换行}"
+ .strn "タﾞメーシﾞを２分の１{换行}"
+ .strn "にへらす{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_025:
+;－－－－－－－－－－－－
+;.strn "ミラー{换行}"
+;.strn "　特になし{结束}"
+;－－－－－－－－－－－－
+ .strn "ミラー{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_026:
+;－－－－－－－－－－－－
+;.strn "メイスﾞ{换行}"
+;.strn "　特になし{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "メイスﾞ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_027:
+;－－－－－－－－－－－－
+;.strn "リターン{换行}"
+;.strn "　相手と自分のほ{换行}"
+;.strn "しﾞょこうかをすへﾞ{换行}"
+;.strn "てテﾞフォルトにも{换行}"
+;.strn "とﾞす{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "リターン{换行}"
+ .strn "　相手と自分のほ{换行}"
+ .strn "しﾞょこうかをすへﾞ{换行}"
+ .strn "てテﾞフォルトにも{换行}"
+ .strn "とﾞす{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_028:
+;－－－－－－－－－－－－
+;.strn "ショット{换行}"
+;.strn "　特になし{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ショット{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_029:
+;－－－－－－－－－－－－
+;.strn "スウィート{换行}"
+;.strn "　特になし{结束}"
+;－－－－－－－－－－－－
+ .strn "スウィート{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_030:
+;－－－－－－－－－－－－
+;.strn "タﾞッシュ{换行}"
+;.strn "　いこﾞ､自分の全て{换行}"
+;.strn "のほﾞうきﾞょカートﾞ{换行}"
+;.strn "のＥに５０たす{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "タﾞッシュ{换行}"
+ .strn "　いこﾞ､自分の全て{换行}"
+ .strn "のほﾞうきﾞょカートﾞ{换行}"
+ .strn "のＥに５０たす{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_031:
+;－－－－－－－－－－－－
+;.strn "ヒﾞックﾞ{换行}"
+;.strn "　特になし{结束}"
+;－－－－－－－－－－－－
+ .strn "ヒﾞックﾞ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_032:
+;－－－－－－－－－－－－
+;.strn "クリエイト{换行}"
+;.strn "　特になし{结束}"
+;－－－－－－－－－－－－
+ .strn "クリエイト{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_033:
+;－－－－－－－－－－－－
+;.strn "チェンシﾞ{换行}"
+;.strn "　いこﾞ､自分の地クﾞ{换行}"
+;.strn "ルーフﾟのほﾞうきﾞょ{换行}"
+;.strn "カートﾞのＥに３０を{换行}"
+;.strn "たす{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "チェンシﾞ{换行}"
+ .strn "　いこﾞ､自分の地クﾞ{换行}"
+ .strn "ルーフﾟのほﾞうきﾞょ{换行}"
+ .strn "カートﾞのＥに３０を{换行}"
+ .strn "たす{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_034:
+;－－－－－－－－－－－－
+;.strn "フリースﾞ{换行}"
+;.strn "　事前にスノウを{换行}"
+;.strn "使うとＥに５０た{换行}"
+;.strn "され､ファイアリー{换行}"
+;.strn "てﾞ５０ひかれる{结束}"
+;－－－－－－－－－－－－
+ .strn "フリースﾞ{换行}"
+ .strn "　事前にスノウを{换行}"
+ .strn "使うとＥに５０た{换行}"
+ .strn "され､ファイアリー{换行}"
+ .strn "てﾞ５０ひかれる{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_035:
+;－－－－－－－－－－－－
+;.strn "ファイアリー{换行}"
+;.strn "　事前にクリエイト{换行}"
+;.strn "を使うと､Ｅに５０た{换行}"
+;.strn "されフリースﾞてﾞ５０{换行}"
+;.strn "ひかれる{结束}"
+;－－－－－－－－－－－－
+ .strn "ファイアリー{换行}"
+ .strn "　事前にクリエイト{换行}"
+ .strn "を使うと､Ｅに５０た{换行}"
+ .strn "されフリースﾞてﾞ５０{换行}"
+ .strn "ひかれる{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_036:
+;－－－－－－－－－－－－
+;.strn "スノウ{换行}"
+;.strn "　しﾞせﾞんにクラウ{换行}"
+;.strn "トﾞを使っておくと､{换行}"
+;.strn "あたえたタﾞメーシﾞに{换行}"
+;.strn "３０かﾞたされる{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "スノウ{换行}"
+ .strn "　しﾞせﾞんにクラウ{换行}"
+ .strn "トﾞを使っておくと､{换行}"
+ .strn "あたえたタﾞメーシﾞに{换行}"
+ .strn "３０かﾞたされる{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_037:
+;－－－－－－－－－－－－
+;.strn "ウﾞォイス{换行}"
+;.strn "　いこﾞ､タﾞメーシﾞ判{换行}"
+;.strn "定のさいのクﾞルーフﾟ{换行}"
+;.strn "こうかをむしする{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ウﾞォイス{换行}"
+ .strn "　いこﾞ､タﾞメーシﾞ判{换行}"
+ .strn "定のさいのクﾞルーフﾟ{换行}"
+ .strn "こうかをむしする{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_038:
+;－－－－－－－－－－－－
+;.strn "ロック{换行}"
+;.strn "　いこﾞ､３ターン自{换行}"
+;.strn "分のほﾞうきﾞょカー{换行}"
+;.strn "トﾞのＥに５０をた{换行}"
+;.strn "し､こうけﾞきカートﾞ{换行}"
+;.strn "から５０をひく{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ロック{换行}"
+ .strn "　いこﾞ､３ターン自{换行}"
+ .strn "分のほﾞうきﾞょカー{换行}"
+ .strn "トﾞのＥに５０をた{换行}"
+ .strn "し､こうけﾞきカートﾞ{换行}"
+ .strn "から５０をひく{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_039:
+;－－－－－－－－－－－－
+;.strn "クラウトﾞ{换行}"
+;.strn "　特になし{结束}"
+;－－－－－－－－－－－－
+ .strn "クラウトﾞ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_040:
+;－－－－－－－－－－－－
+;.strn "トﾞリーム{换行}"
+;.strn "　特になし{结束}"
+;－－－－－－－－－－－－
+ .strn "トﾞリーム{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_041:
+;－－－－－－－－－－－－
+;.strn "サントﾞ{换行}"
+;.strn "　特になし{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "サントﾞ{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_042:
+;－－－－－－－－－－－－
+;.strn "ライト{换行}"
+;.strn "　いこﾞ､自分のかい{换行}"
+;.strn "ふくカートﾞの全て{换行}"
+;.strn "のＥに３０をたす{结束}"
+;－－－－－－－－－－－－
+ .strn "ライト{换行}"
+ .strn "　いこﾞ､自分のかい{换行}"
+ .strn "ふくカートﾞの全て{换行}"
+ .strn "のＥに３０をたす{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_043:
+;－－－－－－－－－－－－
+;.strn "タﾞーク{换行}"
+;.strn "　いこﾞ､相手のかい{换行}"
+;.strn "ふくカートﾞの全て{换行}"
+;.strn "のＥから３０をひく{结束}"
+;－－－－－－－－－－－－
+ .strn "タﾞーク{换行}"
+ .strn "　いこﾞ､相手のかい{换行}"
+ .strn "ふくカートﾞの全て{换行}"
+ .strn "のＥから３０をひく{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_044:
+;－－－－－－－－－－－－
+;.strn "ツイン{换行}"
+;.strn "　相手のほﾞうきﾞょ{换行}"
+;.strn "カートﾞかﾞ水クﾞルーフﾟ{换行}"
+;.strn "のときのみ､タﾞメーシﾞ{换行}"
+;.strn "を１․２５はﾞいにする{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ツイン{换行}"
+ .strn "　相手のほﾞうきﾞょ{换行}"
+ .strn "カートﾞかﾞ水クﾞルーフﾟ{换行}"
+ .strn "のときのみ､タﾞメーシﾞ{换行}"
+ .strn "を１․２５はﾞいにする{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_045:
+;－－－－－－－－－－－－
+;.strn "アーシー{换行}"
+;.strn "　特になし{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "アーシー{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_046:
+;－－－－－－－－－－－－
+;.strn "アロー{换行}"
+;.strn "　風クﾞルーフﾟのほﾞ{换行}"
+;.strn "うきﾞょカートﾞにた{换行}"
+;.strn "いして､タﾞメーシﾞかﾞ{换行}"
+;.strn "１․５はﾞいになる{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "アロー{换行}"
+ .strn "　風クﾞルーフﾟのほﾞ{换行}"
+ .strn "うきﾞょカートﾞにた{换行}"
+ .strn "いして､タﾞメーシﾞかﾞ{换行}"
+ .strn "１․５はﾞいになる{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_047:
+;－－－－－－－－－－－－
+;.strn "ハﾞフﾞル{换行}"
+;.strn "　特になし{结束}"
+;－－－－－－－－－－－－
+ .strn "ハﾞフﾞル{换行}"
+ .strn "　特になし{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_048:
+;－－－－－－－－－－－－
+;.strn "ウェイフﾞ{换行}"
+;.strn "　相手の攻撃カー{换行}"
+;.strn "トﾞかﾞ火クﾞルーフﾟの{换行}"
+;.strn "ときのみ､はん撃タﾞ{换行}"
+;.strn "メーシﾞを１․２５はﾞ{换行}"
+;.strn "いにする{结束}"
+;－－－－－－－－－－－－
+ .strn "ウェイフﾞ{换行}"
+ .strn "　相手の攻撃カー{换行}"
+ .strn "トﾞかﾞ火クﾞルーフﾟの{换行}"
+ .strn "ときのみ､はん撃タﾞ{换行}"
+ .strn "メーシﾞを１․２５はﾞ{换行}"
+ .strn "いにする{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_049:
+;－－－－－－－－－－－－
+;.strn "スルー{换行}"
+;.strn "　いこﾞ､３ターン相{换行}"
+;.strn "手火クﾞルーフﾟの攻{换行}"
+;.strn "撃カートﾞのＥから{换行}"
+;.strn "５０ひく{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "スルー{换行}"
+ .strn "　いこﾞ､３ターン相{换行}"
+ .strn "手火クﾞルーフﾟの攻{换行}"
+ .strn "撃カートﾞのＥから{换行}"
+ .strn "５０ひく{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_050:
+;－－－－－－－－－－－－
+;.strn "ストーム{换行}"
+;.strn "　しﾞせﾞんにレイン{换行}"
+;.strn "を使っておくと､タﾞ{换行}"
+;.strn "メーシﾞかﾞ１․５はﾞい{换行}"
+;.strn "になる{结束}"
+;－－－－－－－－－－－－
+ .strn "ストーム{换行}"
+ .strn "　しﾞせﾞんにレイン{换行}"
+ .strn "を使っておくと､タﾞ{换行}"
+ .strn "メーシﾞかﾞ１․５はﾞい{换行}"
+ .strn "になる{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2,0xFF :: Block0x1FC210_051:
+;－－－－－－－－－－－－
+;.strn "ライフﾞラ{换行}"
+;.strn "　相手かﾞかいふく{换行}"
+;.strn "したホﾟイントをか{换行}"
+;.strn "いふくしていない{换行}"
+;.strn "しﾞょうたいにする{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ライフﾞラ{换行}"
+ .strn "　相手かﾞかいふく{换行}"
+ .strn "したホﾟイントをか{换行}"
+ .strn "いふくしていない{换行}"
+ .strn "しﾞょうたいにする{结束}"
+;－－－－－－－－－－－－
+.endarea
+
+;子文件文本指针
+.org 0x1FC210   ;暂勿改动指针所处地址
+;.align 2,0xFF :: Block0x1FC210_Ptr:
+.area 0x1FC2E0-.        ;原位覆写须限定area
+        .hword (Block0x1FC210_000 & 0xF),(((Block0x1FC210_000 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_001 & 0xF),(((Block0x1FC210_001 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_002 & 0xF),(((Block0x1FC210_002 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_003 & 0xF),(((Block0x1FC210_003 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_004 & 0xF),(((Block0x1FC210_004 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_005 & 0xF),(((Block0x1FC210_005 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_006 & 0xF),(((Block0x1FC210_006 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_007 & 0xF),(((Block0x1FC210_007 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_008 & 0xF),(((Block0x1FC210_008 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_009 & 0xF),(((Block0x1FC210_009 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_010 & 0xF),(((Block0x1FC210_010 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_011 & 0xF),(((Block0x1FC210_011 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_012 & 0xF),(((Block0x1FC210_012 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_013 & 0xF),(((Block0x1FC210_013 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_014 & 0xF),(((Block0x1FC210_014 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_015 & 0xF),(((Block0x1FC210_015 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_016 & 0xF),(((Block0x1FC210_016 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_017 & 0xF),(((Block0x1FC210_017 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_018 & 0xF),(((Block0x1FC210_018 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_019 & 0xF),(((Block0x1FC210_019 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_020 & 0xF),(((Block0x1FC210_020 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_021 & 0xF),(((Block0x1FC210_021 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_022 & 0xF),(((Block0x1FC210_022 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_023 & 0xF),(((Block0x1FC210_023 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_024 & 0xF),(((Block0x1FC210_024 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_025 & 0xF),(((Block0x1FC210_025 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_026 & 0xF),(((Block0x1FC210_026 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_027 & 0xF),(((Block0x1FC210_027 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_028 & 0xF),(((Block0x1FC210_028 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_029 & 0xF),(((Block0x1FC210_029 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_030 & 0xF),(((Block0x1FC210_030 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_031 & 0xF),(((Block0x1FC210_031 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_032 & 0xF),(((Block0x1FC210_032 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_033 & 0xF),(((Block0x1FC210_033 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_034 & 0xF),(((Block0x1FC210_034 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_035 & 0xF),(((Block0x1FC210_035 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_036 & 0xF),(((Block0x1FC210_036 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_037 & 0xF),(((Block0x1FC210_037 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_038 & 0xF),(((Block0x1FC210_038 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_039 & 0xF),(((Block0x1FC210_039 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_040 & 0xF),(((Block0x1FC210_040 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_041 & 0xF),(((Block0x1FC210_041 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_042 & 0xF),(((Block0x1FC210_042 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_043 & 0xF),(((Block0x1FC210_043 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_044 & 0xF),(((Block0x1FC210_044 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_045 & 0xF),(((Block0x1FC210_045 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_046 & 0xF),(((Block0x1FC210_046 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_047 & 0xF),(((Block0x1FC210_047 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_048 & 0xF),(((Block0x1FC210_048 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_049 & 0xF),(((Block0x1FC210_049 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_050 & 0xF),(((Block0x1FC210_050 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC210_051 & 0xF),(((Block0x1FC210_051 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.endarea
+
+
+.func Block0x1FC210_End :: .endfunc
---
+++ strings/Block0x1F/Block0x1FC314.s
@@ -0,0 +1,51 @@
+;Block0x1FC314
+;子文件文本，共4条子文本
+.org 0x1FC2F2 :: .area 0x1FC314-.,0xFF  ;原位覆写须限定area
+
+
+.align 2,0xFF :: Block0x1FC314_000:
+;－－－－－－－－－－－－        
+;.strn "あなたの勝ちてﾞす{结束}{结束}"
+ .strn "あなたの勝ちてﾞす{结束}"
+;－－－－－－－－－－－－ 
+
+
+.align 2,0xFF :: Block0x1FC314_001:
+;－－－－－－－－－－－－        
+;.strn "あなたの負けてﾞす{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "あなたの負けてﾞす{结束}"
+;－－－－－－－－－－－－ 
+
+
+.align 2,0xFF :: Block0x1FC314_002:
+;－－－－－－－－－－－－        
+;.strn "引き分けてﾞす{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "引き分けてﾞす{结束}"
+;－－－－－－－－－－－－ 
+.endarea
+
+.org 0x1FC37A :: .area 0x1FC396-.,0xFF  ;原位覆写须限定area
+.align 2,0xFF :: Block0x1FC314_003:
+;－－－－－－－－－－－－
+;.strn "上下てﾞえらんてﾞ{换行}"
+;.strn "Ⓐホﾞタンてﾞ決定します{结束}"
+;－－－－－－－－－－－－
+ .strn "上下てﾞえらんてﾞ{换行}"
+ .strn "Ⓐホﾞタンてﾞ決定します{结束}"
+;－－－－－－－－－－－－ 
+.endarea
+
+
+;子文件文本指针
+.org 0x1FC314   ;暂勿改动指针所处地址
+        .hword (Block0x1FC314_000 & 0xF),(((Block0x1FC314_000 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC314_001 & 0xF),(((Block0x1FC314_001 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FC314_002 & 0xF),(((Block0x1FC314_002 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        
+.org 0x1FC396   ;暂勿改动指针所处地址
+        .hword (Block0x1FC314_003 & 0xF),(((Block0x1FC314_003 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+
+
+.func Block0x1FC314_End :: .endfunc
