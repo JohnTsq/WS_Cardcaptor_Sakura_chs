cecac5a722de96e430ecabe52a2e1fed91cf3e4d
JohnTsq <msn451518580@hotmail.com>
2025-02-10 20:37:35 +0800

    更新Block0x1F的译文
    翻译 by @zkyhzxf

--- strings/Block0x1F/Block0x1FC58A.s
+++ strings/Block0x1F/Block0x1FC58A.s
@@ -3,107 +3,107 @@
 .org 0x1FC570 :: .area 0x1FC58A-.,0xFF
 .align 2,0xFF :: Block0x1FC58A_000:
 ;.strn "汝のあるへﾞき姿に戻れ！{结束}"
- .strn "汝のあるへﾞき姿に戻れ！{结束}"
+ .strn "恢复你原来的样子！{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_001:
 ;.strn "クロウカートﾞ！{结束}"
- .strn "クロウカートﾞ！{结束}"
+ .strn "库洛牌！{结束}"
 .endarea
 
 .org 0x1FC592 :: .area 0x1FC59E-.,0xFF
 .align 2,0xFF :: Block0x1FC58A_002:
 ;.strn "日っきをつけるんやな{换行}{结束}"
- .strn "日っきをつけるんやな{换行}{结束}"
+ .strn "在这里写日记啊{换行}{结束}"
 .endarea
 
 .org 0x1FC5A2 :: .area 0x1FC5BC-.,0xFF
 .align 2,0xFF :: Block0x1FC58A_003:
 ;.strn "汝のあるへﾞき姿にもとﾞれ{结束}"
- .strn "汝のあるへﾞき姿にもとﾞれ{结束}"
+ .strn "恢复你原来的样子！{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_004:
 ;.strn "クロウカートﾞ！{结束}"
- .strn "クロウカートﾞ！{结束}"
+ .strn "库洛牌！{结束}"
 .endarea
 
 .org 0x1FC5C4 :: .area 0x1FC5E8-.,0xFF
 .align 2,0xFF :: Block0x1FC58A_005:
 ;.strn "闇の力をひめし鍵よ{结束}"
- .strn "闇の力をひめし鍵よ{结束}"
+ .strn "隐藏着黑暗力量的钥匙啊{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_006:
 ;.strn "真の姿を我の前に示せ{结束}"
- .strn "真の姿を我の前に示せ{结束}"
+ .strn "在我面前展示你真正的力量{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_007:
 ;.strn "レリースﾞ{结束}"
- .strn "レリースﾞ{结束}"
+ .strn "封印解除{结束}"
 .endarea
 
 .org 0x1FC5F6 :: .area 0x1FC6B2-.,0xFF
 .align 2,0xFF :: Block0x1FC58A_008:
 ;.strn "オーフﾟニンクﾞ{结束}"
- .strn "オーフﾟニンクﾞ{结束}"
+ .strn "主题曲{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_009:
 ;.strn "夢{结束}"
- .strn "夢{结束}"
+ .strn "梦{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_010:
 ;.strn "育成{结束}"
- .strn "育成{结束}"
+ .strn "养成{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_011:
 ;.strn "にちしﾞょう{结束}"
- .strn "にちしﾞょう{结束}"
+ .strn "日常{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_012:
 ;.strn "ほのほﾞの{结束}"
- .strn "ほのほﾞの{结束}"
+ .strn "暖暖的{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_013:
 ;.strn "きょうふ{结束}"
- .strn "きょうふ{结束}"
+ .strn "恐怖{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_014:
 ;.strn "おまぬけ{结束}"
- .strn "おまぬけ{结束}"
+ .strn "笨蛋{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_015:
 ;.strn "あこかﾞれ{结束}"
- .strn "あこかﾞれ{结束}"
+ .strn "憧憬{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_016:
 ;.strn "ハﾟニック{结束}"
- .strn "ハﾟニック{结束}"
+ .strn "危机{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_017:
 ;.strn "きんはﾟく{结束}"
- .strn "きんはﾟく{结束}"
+ .strn "紧迫{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_018:
 ;.strn "ひあい{结束}"
- .strn "ひあい{结束}"
+ .strn "悲哀{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_019:
 ;.strn "ヒﾟンチ{结束}"
- .strn "ヒﾟンチ{结束}"
+ .strn "危机{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_020:
 ;.strn "お出かけ{结束}"
- .strn "お出かけ{结束}"
+ .strn "外出{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_021:
 ;.strn "夜まつり{结束}"
- .strn "夜まつり{结束}"
+ .strn "夜祭{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_022:
 ;.strn "雪兔とテﾞート{结束}"
- .strn "雪兔とテﾞート{结束}"
+ .strn "与雪兔约会{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_023:
 ;.strn "クロウカートﾞ{结束}"
- .strn "クロウカートﾞ{结束}"
+ .strn "库洛牌ﾞ{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_024:
 ;.strn "封印解除{结束}"
@@ -111,43 +111,43 @@
 
 .align 2,0xFF :: Block0x1FC58A_025:
 ;.strn "戦闘{结束}"
- .strn "戦闘{结束}"
+ .strn "战斗{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_026:
 ;.strn "カートﾞ封印{结束}"
- .strn "カートﾞ封印{结束}"
+ .strn "卡牌封印{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_027:
 ;.strn "最後の審判{结束}"
- .strn "最後の審判{结束}"
+ .strn "最后的审判{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_028:
 ;.strn "ラスホﾞス{结束}"
- .strn "ラスホﾞス{结束}"
+ .strn "最终boss{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_029:
 ;.strn "勝利{结束}"
- .strn "勝利{结束}"
+ .strn "胜利{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_030:
 ;.strn "ショッフﾟ{结束}"
- .strn "ショッフﾟ{结束}"
+ .strn "商店{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_031:
 ;.strn "知世の歌{结束}"
- .strn "知世の歌{结束}"
+ .strn "知世的歌{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_032:
 ;.strn "ふえ{结束}"
- .strn "ふえ{结束}"
+ .strn "笛声{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_033:
 ;.strn "ハﾞットﾞエントﾞ{结束}"
- .strn "ハﾞットﾞエントﾞ{结束}"
+ .strn "坏结局{结束}"
 
 .align 2,0xFF :: Block0x1FC58A_034:
 ;.strn "エンテﾞィンクﾞ{结束}"
- .strn "エンテﾞィンクﾞ{结束}"
+ .strn "通关音乐{结束}"
 .endarea
 
 ;子文件文本指针
