00d930e561880d4619568ed3a5dca18e3dcaef24
JohnTsq <msn451518580@hotmail.com>
2025-02-26 02:26:22 +0800

    调整tilemap扩展名

--- graphic/battle.s
+++ graphic/battle.s
@@ -5,15 +5,15 @@
 
 .org 0x0079E0
 .area 0x007AB8-.
-    .incbin ".\graphic\battle\MapOfBattleMenu1.map"     ;0x0079E0
+    .incbin ".\graphic\battle\MapOfBattleMenu1.tilemap"     ;0x0079E0
 .endarea
 
 .org 0x007ABC
 .area 0x007B94-.
-    .incbin ".\graphic\battle\MapOfBattleMenu2.map"     ;0x007ABC
+    .incbin ".\graphic\battle\MapOfBattleMenu2.tilemap"     ;0x007ABC
 .endarea
 
 .org 0x0902C6
     .incbin ".\graphic\battle\baserom_902C6h_230h.2bpp"
 .org 0x0904FA
-    .incbin ".\graphic\battle\baserom_904FAh_80h.map"
+    .incbin ".\graphic\battle\baserom_904FAh_80h.tilemap"
