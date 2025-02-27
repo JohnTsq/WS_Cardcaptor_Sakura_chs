c7b51813528669e601154858b504be8a469ff15f
JohnTsq <msn451518580@hotmail.com>
2025-02-28 00:00:28 +0800

    汉化标题菜单图像

---
+++ graphic/title.s
@@ -0,0 +1,18 @@
+.org 0x0D9696
+    .incbin ".\graphic\title\menu\baserom_D9696h_1140h.2bpp"
+.org 0x0DA7DA
+    .incbin ".\graphic\title\menu\baserom_DA7DAh_FCh.tilemap"
+.org 0x0DA8DA
+    .incbin ".\graphic\title\menu\baserom_DA8DAh_150h.tilemap"
+.org 0x0DAA2E
+    .incbin ".\graphic\title\menu\baserom_DAA2Eh_1A4h.tilemap"
+.org 0x0DABD6
+    .incbin ".\graphic\title\menu\baserom_DABD6h_150h.tilemap"
+.org 0x0DAD2A
+    .incbin ".\graphic\title\menu\baserom_DAD2Ah_198h.tilemap"
+.org 0x0DAEC6
+    .incbin ".\graphic\title\menu\baserom_DAEC6h_180h.tilemap"
+.org 0x0DB04A
+    .incbin ".\graphic\title\menu\baserom_DB04Ah_1C8h.tilemap"
+.org 0x0DB216
+    .incbin ".\graphic\title\menu\baserom_DB216h_6Ch.tilemap"
