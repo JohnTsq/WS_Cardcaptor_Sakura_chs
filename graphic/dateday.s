c2394dad90b70cc85a7c6882a5c470690e87b2ce
JohnTsq <msn451518580@hotmail.com>
2025-03-23 11:29:14 +0800

    调整日期图像语序
    调整硬编码于程序中的tilemap拷贝的目标地址。

--- graphic/dateday.s
+++ graphic/dateday.s
@@ -2,3 +2,5 @@
     .incbin ".\graphic\dateday\baserom_D015Ah_8F0h.2bpp"
 .orga 0x0D0AFA
     .incbin ".\graphic\dateday\baserom_D0AFAh_3F0h.tilemap"
+.orga 0x1DD036
+    .byte 0xB9,0xE8,0x11    ; mov cx, 0x11E8
