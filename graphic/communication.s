fc775ecb1691d159fa4e2e596fa8def76d41a1f9
JohnTsq <msn451518580@hotmail.com>
2025-04-20 16:09:19 +0800

    汉化图像
    - 联机对战界面文本
    - “好盆友”物品图标

--- graphic/communication.s
+++ graphic/communication.s
@@ -1,9 +1,15 @@
+.orga 0x08D14E
+    .incbin ".\graphic\communication\baserom_8D14Eh_AF0h.2bpp"
+
 .orga 0x0CB55A
     .incbin ".\graphic\communication\baserom_CB55Ah_390h.2bpp"
 .orga 0x0CB8EE
     .incbin ".\graphic\communication\baserom_CB8EEh_80h.tilemap"
 .orga 0x0CB972
     .incbin ".\graphic\communication\baserom_CB972h_80h.tilemap"
+
+.orga 0x0CBA12
+    .incbin ".\graphic\communication\baserom_CBA12h_AF0h.2bpp"
 
 .orga 0x0CD89E
     .incbin ".\graphic\communication\baserom_CD89Eh_4D0h.2bpp"
@@ -13,3 +19,8 @@
     .incbin ".\graphic\communication\baserom_CDE3Eh_C8h.tilemap"
 .orga 0x0CDF0A
     .incbin ".\graphic\communication\baserom_CDF0Ah_CCh.tilemap"
+
+.orga 0x134396
+    .incbin ".\graphic\communication\baserom_134396h_140h.2bpp"
+.orga 0x1344DA
+    .incbin ".\graphic\communication\baserom_1344DAh_3F0h.tilemap"
